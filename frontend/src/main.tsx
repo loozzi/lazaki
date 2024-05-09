2e3607543ede9b90f272fb89fda43d11a5affd11
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-09 11:54:18 +0700

    feat: install nextui & tailwindcss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
+import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import App from '~/App'
-import './index.css'
 import { history } from '~/configs/history'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HistoryRouter history={history}>
-      <App />
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
     </HistoryRouter>
   </React.StrictMode>
 )

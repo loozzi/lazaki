881ab847f39181dc3323e7c585c491cb07b0d7df
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-09 10:19:50 +0700

    feat: add history config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+import App from '~/App'
 import './index.css'
+import { history } from '~/configs/history'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HistoryRouter history={history}>
+      <App />
+    </HistoryRouter>
+  </React.StrictMode>
 )

0970f5a10822f0c67d13567b247f6ed69533835d
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-12 00:16:17 +0700

    feat: config sql & route api

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,10 +1,19 @@
-from dotenv import load_dotenv
 from flask import Flask
-from src.config import _config
-
-load_dotenv()
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from src.config import _config, envConfig
+from src.models import *
+from src.utils import api
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = envConfig.SQLALCHEMY_DATABASE_URI
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
 config = _config.getDevConfig()
 app.env = config.ENV
+
+app.register_blueprint(api, url_prefix="/api")

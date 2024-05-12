649c7e37038ed9dbbc921930cb49a3b0ff3e7827
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-12 14:56:34 +0700

    fix: relation of product category

--- backend/src/models/CategoryProduct.py
+++ backend/src/models/CategoryProduct.py
@@ -1,23 +1,13 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
-
-if TYPE_CHECKING:
-    from .Category import Category
-    from .Product import Product
+from src.models.Base import Base
 
 
-class CategoryProduct:
+class CategoryProduct(Base):
     __tablename__ = "category_product"
     categoryId: Mapped[int] = mapped_column(
         Integer, ForeignKey("category.id"), primary_key=True
     )
     productId: Mapped[int] = mapped_column(
         Integer, ForeignKey("product.id"), primary_key=True
-    )
-
-    product: Mapped["Product"] = mapped_column(ForeignKey("product.id"), nullable=True)
-    category: Mapped["Category"] = mapped_column(
-        ForeignKey("category.id"), nullable=True
     )

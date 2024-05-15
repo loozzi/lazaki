7c6fd8dbbc35dd504ea163c8121519a7b92a0be4
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-16 00:17:10 +0700

    feat: init route

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,7 +1,19 @@
 from flask import Blueprint
 
+from .admin_route import admin
 from .auth_route import auth
+from .category_route import category
+from .order_route import order
+from .payment_route import payment
+from .product_route import product
+from .user_route import user
 
 api = Blueprint("api", __name__)
 
+api.register_blueprint(admin, url_prefix="/admin")
 api.register_blueprint(auth, url_prefix="/auth")
+api.register_blueprint(category, url_prefix="/category")
+api.register_blueprint(order, url_prefix="/order")
+api.register_blueprint(payment, url_prefix="/payment")
+api.register_blueprint(product, url_prefix="/product")
+api.register_blueprint(user, url_prefix="/user")

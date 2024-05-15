7ac41d045524ea3186a82125c8e0f4410ac9871b
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-15 17:53:58 +0700

    modify: add fk token customer

--- backend/src/models/Token.py
+++ backend/src/models/Token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 from src.models.Base import Base
 
@@ -6,9 +6,11 @@ from src.models.Base import Base
 class Token(Base):
     __tablename__ = "token"
     uid: Mapped[str] = mapped_column(
-        String(512), nullable=False, unique=True, primary_key=True
+        ForeignKey("customer.uid"),
+        nullable=False,
+        primary_key=True,
     )
-    token: Mapped[str] = mapped_column(String(512), nullable=False)
+    token: Mapped[str] = mapped_column(String(2048), nullable=False)
 
     def setToken(self, token: str):
         self.token = token

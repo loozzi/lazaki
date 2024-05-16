16a5b7caf23d8f8820084fb2e7baa4c48698d3bc
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-16 13:55:43 +0700

    feat: add pagination middleware

--- backend/src/controllers/Pagination.py
+++ backend/src/controllers/Pagination.py
@@ -1,4 +1,7 @@
+from functools import wraps
 from typing import List
+
+from flask import request
 
 
 class Pagination:
@@ -7,3 +10,23 @@ class Pagination:
         self.perPage = perPage
         self.total = total
         self.data = data
+
+    def serialize(self):
+        return {
+            "currentPage": self.currentPage,
+            "perPage": self.perPage,
+            "total": self.total,
+            "data": self.data,
+        }
+
+
+def request_pagination(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        page = request.args.get("page", 1, type=int)
+        limit = request.args.get("limit", 10, type=int)
+        request.pagination = {"page": max(page, 1), "limit": max(limit, 10)}
+
+        return f(*args, **kwargs)
+
+    return decorated

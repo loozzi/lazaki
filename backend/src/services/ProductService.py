72e0e0e73f8bf5e1aeb6f1ed815ac6a69d93e80e
unknown <vuthanhdat019@gmail.com>
2024-05-19 00:46:42 +0700

    fix bug response & update model recommend

--- backend/src/services/ProductService.py
+++ backend/src/services/ProductService.py
@@ -7,35 +7,47 @@ from src.models.Category import Category
 from src.models.CategoryProduct import CategoryProduct
 from sqlalchemy import or_, and_ , desc, asc
 from src.controllers.Pagination import Pagination
+from src.services.RecommendService import RecommendService
+from src.utils.response import Response
 
 class ProductService:
+
+
+    def data_response(self, list_product: List[Product], sort: str):
+            data = []
+            for product in list_product:
+                info_product = product.serialize()
+                data_one_product = {}
+                data_one_product["name"] = info_product["name"]
+                data_one_product["slug"] = product.slug
+                data_one_product["image"] = product.getPrimaryImage()
+                data_one_product["price"] = info_product["variations"][0]["price"]
+                sold = 0
+                for variation in info_product["variations"]:
+                    sold += variation["sold"]
+                data_one_product["sold"] = sold
+                data_one_product["rating"] = product.getRateMean()
+                data.append(data_one_product)
+            if sort == "desc":
+                data = sorted(data,
+                            key=lambda x: x["price"], reverse=True)
+            else:
+                data = sorted(data,
+                            key=lambda x: x["price"])
+            return data
+
     # Lấy danh sách sản phẩm
-    def getProducts(sort, limit: int, page: int):
+    def getProducts(self, sort, limit: int, page: int):
         all_products = db.session.query(Product).filter(
                             Product.isDeleted == False).all()
-        data = []
-        for product in all_products:
-            info_product = product.serialize()
-            data_one_product = {}
-            data_one_product["name"] = info_product["name"]
-            data_one_product["slug"] = product.slug
-            data_one_product["image"] = product.getPrimaryImage()
-            data_one_product["price"] = info_product["variations"][0]["price"]
-            data_one_product["rating"] = product.getRateMean()
-            data.append(data_one_product)
-        if sort == "desc":
-            data = sorted(data,
-                          key=lambda x: x["price"], reverse=True)
-        else:
-            data = sorted(data,
-                          key=lambda x: x["price"])
+        data = self.data_response(all_products, sort)
         data = data[(page-1)*limit: limit + (page-1)*limit]
         res = Pagination(page, len(data), len(all_products), data)
         return res.serialize()
 
 
     # Tìm kiếm sản phẩm
-    def searchProducts(keyword: str, minPrice: int,
+    def searchProducts(self, keyword: str, minPrice: int,
                        maxPrice: int, categories: List[str], sort: str,
                        limit: int, page: int):
         if len(categories)  == 0:
@@ -53,7 +65,8 @@ class ProductService:
                             Variation.price >= minPrice).filter(
                                 Variation.price <= maxPrice).order_by(
                                         desc(Variation.price)).all()
-                product_list = [product.serialize() for product in products_join]
+                product_list = [product for product in products_join]
+                product_list = self.data_response(product_list, sort)
                 result = product_list[(page-1)*limit: limit + (page-1)*limit]
                 product_pagination = Pagination(page, len(result),
                                                 len(total_products_join), result)
@@ -66,11 +79,13 @@ class ProductService:
                             Variation.price >= minPrice).filter(
                                 Variation.price <= maxPrice).order_by(
                                         asc(Variation.price)).all()
-                product_list = [product.serialize() for product in products_join]
+                product_list = [product for product in products_join]
+                product_list = self.data_response(product_list, sort)
                 result = product_list[(page-1)*limit: limit + (page-1)*limit]
                 product_pagination = Pagination(page, len(result),
                                                 len(total_products_join), result)
                 return product_pagination.serialize()
+
         else:
             comfort_categories = Category.query.filter(
                                     Category.name.in_(categories)).all()
@@ -95,7 +110,8 @@ class ProductService:
                                 Variation.price <= maxPrice).filter(
                                     Product.id.in_(product_ids)).order_by(
                                         desc(Variation.price)).all()
-                product_list = [product.serialize() for product in products_join]
+                product_list = [product for product in products_join]
+                product_list = self.data_response(product_list, sort)
                 result = product_list[(page-1)*limit: limit + (page-1)*limit]
                 product_pagination = Pagination(page, len(result),
                                                 len(total_products_join), result)
@@ -109,7 +125,8 @@ class ProductService:
                                 Variation.price <= maxPrice).filter(
                                     Product.id.in_(product_ids)).order_by(
                                         asc(Variation.price)).all()
-                product_list = [product.serialize() for product in products_join]
+                product_list = [product for product in products_join]
+                product_list = self.data_response(product_list, sort)
                 result = product_list[(page-1)*limit: limit + (page-1)*limit]
                 product_pagination = Pagination(page, len(result),
                                                 len(total_products_join), result)
@@ -117,7 +134,7 @@ class ProductService:
 
 
     # Lấy chi tiết của sản phẩm
-    def getDetailProduct(productSlug: str):
+    def getDetailProduct(self, productSlug: str):
         product = db.session.query(Product).filter(
                     Product.slug == productSlug).filter(
                         Product.isDeleted == False).first()
@@ -126,7 +143,7 @@ class ProductService:
         return product.serialize()
 
     # Thêm sản phẩm vào database
-    def addProduct(
+    def addProduct(self,
         productName: str,
         description: str,
         properties: List[object],
@@ -135,7 +152,7 @@ class ProductService:
     ):
         pass
 
-    def editProduct(
+    def editProduct(self,
         productId: int,
         productName: str,
         slug: str,
@@ -148,8 +165,22 @@ class ProductService:
         pass
 
     # Xoá sản phẩm khỏi database
-    def removeProduct(productId: int):
+    def removeProduct(self, productId: int):
         pass
 
-    def generateProducts():
-        pass
+    def generateProducts(self, list_slug_accessed: List[str], order_list_customer: List[OrderDetail], limit: int, page: int):
+        list_products = []
+        for order in order_list_customer:
+            product = Product.query.filter_by(id=order.productId).first()
+            if product is not None:
+                list_products.append(product)
+        for slug in list_slug_accessed:
+            product = Product.query.filter_by(slug=slug).first()
+            if product is not None:
+                list_products.append(product)
+        recommend_service = RecommendService()
+        data = recommend_service.generateProducts(list_products)
+        data = self.data_response(data, "asc")
+        result = data[(page-1)*limit: limit + (page-1)*limit]
+        product_pagination = Pagination(page, len(result),len(data), result)
+        return product_pagination.serialize()
--- backend/src/services/RecommendService.py
+++ backend/src/services/RecommendService.py
@@ -1,4 +1,75 @@
+from src.models.Product import Product
+from typing import List
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+
 class RecommendService:
     # Giới thiệu sản phẩm người dùng có thể thích
-    def generateProducts():
-        pass
+
+    def create_dataframe(self, list_product: List[Product]):
+        list_name = []
+        list_price = []
+        list_rating_average= []
+        list_category = []
+        list_solds = []
+        for product in list_product:
+            info = product.serialize()
+            list_name.append(info["name"])
+            list_price.append(info["variations"][0]["price"])
+            list_rating_average.append(product.getRateMean())
+            list_category.append(info["categories"][0]["name"])
+            list_solds.append(info["variations"][0]["sold"])
+        new_data_frame = pd.DataFrame({'name': list_name, 'price': list_price, 'rating_average': list_rating_average, 'category': list_category, 'solds': list_solds})
+        return new_data_frame
+
+
+
+    def prepare_pipe(self, data_frame_product: pd.DataFrame):
+        numeric_cols = data_frame_product[['price', 'rating_average', 'solds']]
+        object_cols = data_frame_product[['category', 'name']]
+        column_trans_numeric = ColumnTransformer(
+                transformers=[
+                    ("price_scaler", StandardScaler(with_mean=False, with_std=True),["price"]),
+                    ("sold_scale",StandardScaler(with_mean=False, with_std=True),["solds"]),
+                    ("rating_scale",StandardScaler(with_mean=False, with_std=True),["rating_average"]),
+                ], remainder="passthrough")
+        column_trans_numeric.fit(numeric_cols)
+        column_trans_object = ColumnTransformer([("category", OneHotEncoder(dtype=int),["category"]),
+                                             ("name", OneHotEncoder(dtype=int), ["name"])], remainder="passthrough")
+        column_trans_object.fit(object_cols)
+        id_nums = []
+        for i in numeric_cols.columns:
+            id_nums.append(data_frame_product.columns.get_loc(i))
+        id_cate = []
+        for i in object_cols.columns:
+            id_cate.append(data_frame_product.columns.get_loc(i))
+        preprocessor = ColumnTransformer([
+            ("num_col", column_trans_numeric, id_nums),
+            ("obj_col", column_trans_object, id_cate),
+        ])
+        PreparePipe = make_pipeline(preprocessor)
+        return PreparePipe
+
+
+    def generateProducts(self, list_product: List[Product]):
+        products_recommend = []
+        all_product = Product.query.filter(Product.isDeleted == False).all()
+        data_frame_all_product = self.create_dataframe(all_product)
+        prepare_pipe = self.prepare_pipe(data_frame_all_product)
+        model_recommend = KMeans(n_clusters=100)
+        model_pipe = make_pipeline(prepare_pipe, model_recommend)
+        model_pipe.fit(data_frame_all_product)
+        data_frame_list_product = self.create_dataframe(list_product)
+        cluster_all_product = model_pipe.predict(data_frame_all_product)
+        cluster_set_product = set(model_pipe.predict(data_frame_list_product))
+        for i in range(len(cluster_all_product)):
+            if cluster_all_product[i] in cluster_set_product:
+                products_recommend.append(all_product[i])
+            if len(products_recommend) == 30:
+                break
+        return products_recommend
+        

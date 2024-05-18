cd01fc9e790a19b50ae63208e030155d0a9aad87
trungnguyen22022665 <130639480+trungnguyen22022665@users.noreply.github.com>
2024-05-19 00:23:48 +0700

    Fix testing env_config.py

--- backend/src/config/env_config.py
+++ backend/src/config/env_config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 class EnvConfig:
     def __init__(self) -> None:
-        self.SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI_TEST")
+        self.SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
         self.SECRET_KEY = os.environ.get("SECRET_KEY")

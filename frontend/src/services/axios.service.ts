23914da9848bd9e012f9958511c43bbf7cdf183e
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-19 01:23:34 +0700

    feat: modify axios interceptors

--- frontend/src/services/axios.service.ts
+++ frontend/src/services/axios.service.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import apiConfig from '~/configs/api.config'
 import envConfig from '~/configs/env.config'
+import tokenService from './token.service'
+import authService from './auth.service'
 
 const client = axios.create({
   baseURL: envConfig.API_ENDPOINT,
@@ -11,6 +14,29 @@ const client = axios.create({
 
 client.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
+    const whiteList = [apiConfig.auth.signIn, apiConfig.auth.loginAdmin, apiConfig.auth.refreshToken]
+
+    const pass: boolean = whiteList.some((url) => config.url?.includes(url))
+
+    if (pass) {
+      return config
+    }
+
+    const accessToken = tokenService.getAccessToken()
+    const refreshToken = tokenService.getRefreshToken()
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
+    } else if (refreshToken) {
+      authService.refreshToken({ token: refreshToken }).then((res) => {
+        if (res.status === 200) {
+          tokenService.signIn(res.data!.accessToken, res.data!.refreshToken)
+        }
+      })
+    }
+
+    const _accessToken = tokenService.getAccessToken()
+    if (_accessToken) config.headers.Authorization = `Bearer ${_accessToken}`
     return config
   },
   (error) => {

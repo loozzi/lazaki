4dfe0e9decbfe7832870918c529ebfb359432500
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-19 14:25:21 +0700

    fix pagination middleware

--- backend/src/middlewares/PaginationMiddleware.py
+++ backend/src/middlewares/PaginationMiddleware.py
@@ -8,8 +8,8 @@ def request_pagination(f):
     def decorated(*args, **kwargs):
         page = request.args.get("page", 1, type=int)
         limit = request.args.get("limit", 10, type=int)
-        request.pagination = {"page": max(page, 1),
-                              "limit": min(limit, 10)}
+
+        request.pagination = {"page": max(page, 1), "limit": max(min(limit, 100), 1)}
 
         return f(*args, **kwargs)
 

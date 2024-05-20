78ed962a3f15f8412574830df7f7e79b530262fc
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-20 11:37:00 +0700

    feat: call api get reviews

--- frontend/src/components/review/review.tsx
+++ frontend/src/components/review/review.tsx
@@ -10,13 +10,19 @@ export const ViewReviewComp = (props: ViewReviewProps) => {
   const { review } = props
   return (
     <div className='flex flex-col mb-4 p-4 md:mr-4 border-solid border-black-1/10 border-1 rounded-xl'>
-      <StarComp stars={review.value} hideNumber />
       <span className='flex items-center'>
         <span className='font-semibold'>{review.fullName}</span>
         <LuDot />
-        <span className='text-gray-400'>{parseDateToDMY(review.created_at)}</span>
+        <span className='text-gray-400'>{parseDateToDMY(review.createdAt)}</span>
       </span>
-      <span className='my-2'>{review.content}</span>
+      <span className='my-1 text-gray-400 flex gap-2 items-center'>
+        <StarComp stars={review.value} hideNumber />
+        <LuDot />
+        {review.variation?.type} - {review.variation?.name}
+      </span>
+      <span className='my-2'>
+        {review.content ? <p>{review.content}</p> : <p className='text-gray-400'>Không có nội dung</p>}
+      </span>
       {review.images && (
         <div className='flex flex-wrap gap-2'>
           {review.images.map((image: string, index: number) => (
